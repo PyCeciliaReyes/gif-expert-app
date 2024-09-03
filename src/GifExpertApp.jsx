2f883d731a97a370530a3a488d18ee3674dbdcb2
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-03 11:39:59 -0400

    agregando categorias usando en setCategories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 export const  GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+    
+    const onAddCategory = () =>{
+        // console.log('valorant')
+        setCategories( [...categories, 'Valorant']);
+        // setCategories(cat => [...categories, 'valorant 2'])
+    }
+
     return(
         <>
             {/* titulo */}
             <h1>GifExpertApp</h1>
             {/* input */}
             {/* listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                { 
+                categories.map(category =>{return <li key={category}> {category}</li>
+                })
+                }
+            </ol>
                 {/* gif item */}
         </>
     )
